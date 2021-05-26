44bf5416da0af3718315800564baedd11bbd0647
Constantine M <slink2111@users.noreply.github.com>
2021-05-26 15:44:59 +0300

    Update emvolia.py
    we search more dates in the second appointment date

--- emvolia.py
+++ emvolia.py
@@ -39,36 +39,44 @@ fp.close()
 day = 26
 
 from datetime import datetime
+
 with open('emvoliastika.json', 'r', encoding="utf8") as fp:
     reader = json.load(fp)
 
     for key in reader:
         # print(key[1])
         # print(key[3])
-        
+
         # because the start date might not be available, we search for a range of days after the start date (be carefull if month changes also)
-        for i in range(0,6):
+        for i in range(0, 6):
             response = requests.post("https://emvolio.gov.gr/app/api/CovidService/CV_TimeSlots_Free",
                                      data={"centerId": key[1], "personId": personId, "firstDoseDate": "null",
-                                           "zoneNum": "null", "selectedDate": '2021-05-'+str(day+i)+'T00:00:00+03:00', "dose": "1",
+                                           "zoneNum": "null",
+                                           "selectedDate": '2021-05-' + str(day + i) + 'T00:00:00+03:00', "dose": "1",
                                            "requestRecommended": "true"}, headers=my_headers)
             if 'exceptionObject' not in response.json().keys():
                 # print(response.json())
-                if response.json()['doses'] ==2 and response.json()['timeslotsFree'][0]['percentAvailable'] > 0:
-                    response2 = requests.post("https://emvolio.gov.gr/app/api/CovidService/CV_TimeSlots_Free",
-                                             data={"centerId": key[1], "personId": personId,
-                                                   "firstDoseDate": response.json()['timeslotsFree'][0]['onDate'],
-                                                   "zoneNum": "null", "selectedDate": "2021-05-29T21:00:00.000Z",
-                                                   "dose": "2", "requestRecommended": "true"}, headers=my_headers)
-                    
-                    # if we can get a start date we try to get a date for the second vaccination apointment 
-                    date_time_0 = datetime.strptime(response.json()['timeslotsFree'][0]['onDate'], '%Y-%m-%dT%H:%M:%S+03:00')
-                    date_time_1 = datetime.strptime(response2.json()['timeslotsFree'][0]['onDate'], '%Y-%m-%dT%H:%M:%S+03:00')
+                for i, percent in enumerate(response.json()['timeslotsFree']):
+                    if response.json()['timeslotsFree'][i]['percentAvailable'] > 0:
+                        response2 = requests.post("https://emvolio.gov.gr/app/api/CovidService/CV_TimeSlots_Free",
+                                                  data={"centerId": key[1], "personId": personId,
+                                                        "firstDoseDate": response.json()['timeslotsFree'][i]['onDate'],
+                                                        "zoneNum": "null", "selectedDate": "2021-05-29T21:00:00.000Z",
+                                                        "dose": "2", "requestRecommended": "true"}, headers=my_headers)
+                        # if we can get a start date we try to get a date for the second vaccination apointment
+                        for j, percents in enumerate(response2.json()['timeslotsFree']):
+                            if response2.json()['timeslotsFree'][j]['percentAvailable'] > 0:
+                                date_time_0 = datetime.strptime(response.json()['timeslotsFree'][i]['onDate'],
+                                                                '%Y-%m-%dT%H:%M:%S+03:00')
+                                date_time_1 = datetime.strptime(response2.json()['timeslotsFree'][j]['onDate'],
+                                                                '%Y-%m-%dT%H:%M:%S+03:00')
 
-                    delta = date_time_1 - date_time_0
-                    # we print the vaccination centers that the available days between the two possible apointments is less than 50 (ie non Astra Zeneca)
-                    if (delta.days < 50):
-                        print(key)
-                        print(delta.days)
+                                delta = date_time_1 - date_time_0
+                                # we print the vaccination centers that the available days between the two possible apointments is less than 50 (ie non Astra Zeneca)
+                                # if (delta.days < 50):
+                                print(key)
+                                print(delta.days)
+                                break
+                        break
                 break
 
