92e9d78a6adc120dc1c2b5d162d688850c9dce25
Constantine M <slink2111@users.noreply.github.com>
2021-05-26 15:56:50 +0300

    Update emvolia.py

--- emvolia.py
+++ emvolia.py
@@ -74,8 +74,8 @@ with open('emvoliastika.json', 'r', encoding="utf8") as fp:
                                 delta = date_time_1 - date_time_0
                                 # we print the vaccination centers that the available days between the two possible apointments is less than 50 (ie POSSIBLE non Astra Zeneca)
                                 if (delta.days < 50):
-                                    print("Vaccination center: " + key[0] + " PK: " + key[2] + " available date of 1st vaccination " + response.json()['timeslotsFree'][i]['onDate'])
-                                    print("Days between vaccination appointments " + delta.days)
+                                    print("Vaccination center: " + str(key[0]) + " PK: " + str(key[2]) + "available date of 1st vaccination " + response.json()['timeslotsFree'][i]['onDate'])
+                                    print("Days between vaccination appointments " + str(delta.days))
                                     break
                         break
                 break
